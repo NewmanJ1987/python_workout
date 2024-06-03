14a6455acf55dd48852092890c6f9b673ed39431
Newman Thurairatnam <newman.thurairatnam@gameloft.com>
2024-06-03 00:02:54 -0400

    fix bug with attempts

--- numeric_types.py
+++ numeric_types.py
@@ -1,8 +1,9 @@
 
 import random
-from typing import Optional
+from typing import Optional, Tuple
 guess_number_prompt: str = "Please enter a number between 1 and 100: "
 random_base: int = random.randint(2,16)
+
 
 def setup_game() -> int:
     random_number: int = random.randint(1, 100)
@@ -14,15 +15,15 @@ def setup_game() -> int:
 def start_game(random_number: int, max_tries: Optional[int] = None ) -> None:
     attempts: int = 1
     user_input: int = int(input(guess_number_prompt), base=random_base)
-    user_input: int = play_game(random_number, max_tries, attempts, user_input)
+    user_input, attempts = play_game(random_number, max_tries, attempts, user_input)
     determine_if_player_won(user_input, random_number, attempts)
 
-def play_game(random_number:int , max_tries: int , attempts: int, user_input: int)-> int:
+def play_game(random_number:int , max_tries: int , attempts: int, user_input: int)-> Tuple[int, int]:
     while user_input != random_number and is_under_max_tries(max_tries, attempts):
         evaluate_guess(random_number, user_input)
         user_input: int = int(input(guess_number_prompt), base=random_base)
         attempts += 1
-    return user_input
+    return user_input, attempts
 
 def evaluate_guess(random_number: int, user_input: int) -> None:
     if user_input > random_number:
