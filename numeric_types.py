3da623d70a6dc08fc028e97a08cf3ea96b34d3e8
Newman Thurairatnam <newman.thurairatnam@gameloft.com>
2024-06-02 23:56:45 -0400

    made one of the variable global

--- numeric_types.py
+++ numeric_types.py
@@ -1,26 +1,26 @@
 
 import random
-from typing import Optional, Tuple
+from typing import Optional
 guess_number_prompt: str = "Please enter a number between 1 and 100: "
+random_base: int = random.randint(2,16)
 
-def setup_game() -> Tuple[int, int]:
+def setup_game() -> int:
     random_number: int = random.randint(1, 100)
-    random_base: int = random.randint(2,16)
     name: str = input("What is your name? ")
     print(f"Hello, {name}! I'm thinking of a number between 1 and 100. Enter the number in base {random_base}.")
-    return random_number, random_base
+    return random_number
     
         
-def start_game(random_number: int, random_base: int, max_tries: Optional[int] = None ) -> None:
+def start_game(random_number: int, max_tries: Optional[int] = None ) -> None:
     attempts: int = 1
     user_input: int = int(input(guess_number_prompt), base=random_base)
-    user_input: int = play_game(random_number, max_tries, attempts, user_input, random_base=random_base)
+    user_input: int = play_game(random_number, max_tries, attempts, user_input)
     determine_if_player_won(user_input, random_number, attempts)
 
-def play_game(random_number:int , max_tries: int , attempts: int, user_input: int, random_base:int)-> int:
+def play_game(random_number:int , max_tries: int , attempts: int, user_input: int)-> int:
     while user_input != random_number and is_under_max_tries(max_tries, attempts):
         evaluate_guess(random_number, user_input)
-        user_input: int = int(input(guess_number_prompt), random_base=random_base)
+        user_input: int = int(input(guess_number_prompt), base=random_base)
         attempts += 1
     return user_input
 
@@ -44,10 +44,8 @@ def is_under_max_tries(max_tries: int, attempts: int) -> bool:
     
     
 def main() -> None:
-    random_number: int
-    random_base: int  
-    random_number, random_base, = setup_game()
-    start_game(random_number, random_base, 3) 
+    random_number: int = setup_game()
+    start_game(random_number,  3) 
     
 
 
